85f7f3294dc7ae84bb6de937b9a735fcd5adc15b
Bugaddr <75427894+Bugaddr@users.noreply.github.com>
2025-03-03 01:28:03 +0530

    Revamp

--- scripts.js
+++
@@ -1,5 +0,0 @@
-function openAllLinks(categoryId, event) {
-    event.stopPropagation(); // Prevent details toggle when clicking the button
-    let links = document.querySelectorAll(`#${categoryId} a`);
-    links.forEach(link => window.open(link.href, '_blank'));
-}
